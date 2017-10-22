1bbbaccbcd383895fa93993afd03cda7576cc4c4
Akshay Dahiya <xadahiya@gmail.com>
2017-10-22 16:29:52 +0530

    Search interface implemented, everything working

--- src/App.js
+++ src/App.js
@@ -1,95 +1,108 @@
 import React from 'react'
 import * as BooksAPI from './BooksAPI'
 import './App.css'
-import { Link, Route } from 'react-router-dom';
+import {Link, Route} from 'react-router-dom';
 
 class BooksApp extends React.Component {
   state = {
-    books : [],
+    books: [],
+    searchResults: []
   }
-  componentDidMount(){
+
+  componentDidMount() {
     BooksAPI.getAll().then((books) => {
       this.setState({books})
-      // console.log(this.state);
     })
-
   }
 
-  updateBookShelf(book, shelf){
+  updateBookShelf(book, shelf) {
 
-    console.log(book, shelf)
     BooksAPI.update(book, shelf).then((b) => {
       book.shelf = shelf
       this.setState((state) => ({
-        books : this.state.books.filter((b) => {
+        books: this.state.books.filter((b) => {
           return b.id !== book.id
         }).concat([book])
       }))
-
     })
-
-
   }
-  returnBooksFromState = (shelf) => {
-    return (this.state.books.filter((book) => {
-        return book.shelf === shelf
-      }).map((book) => {
-        return (
 
+  filterBooksUsingShelf = (books, shelf) => {
+    return books.filter((book) => {
+      return book.shelf === shelf
+    })
+  }
+
+  renderBooks = (books) => {
+    return (books.map((book) => {
+      return (
 
         <li key={book.id}>
-        <div className="book">
-        <div className="book-top">
-        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url(' + book.imageLinks.thumbnail+ ')' }}></div>
-        <div className="book-shelf-changer">
-        <select value={book.shelf} onChange={(event) => {this.updateBookShelf(book, event.target.value)}}>
-        <option value="none" disabled>Move to...</option>
-        <option value="currentlyReading" >Currently Reading</option>
-        <option value="wantToRead">Want to Read</option>
-        <option value="read">Read</option>
-        <option value="none">None</option>
-        </select>
-        </div>
-        </div>
-        <div className="book-title">{book.title}</div>
-        <div className="book-authors">{book.authors[0]}</div>
-        </div>
+          <div className="book">
+            <div className="book-top">
+              <div className="book-cover" style={{
+                width: 128,
+                height: 193,
+                backgroundImage: 'url(' + book.imageLinks.thumbnail + ')'
+              }}></div>
+              <div className="book-shelf-changer">
+                <select value={book.shelf} onChange={(event) => {
+                  this.updateBookShelf(book, event.target.value)
+                }}>
+                  <option value="none" disabled>Move to...</option>
+                  <option value="currentlyReading">Currently Reading</option>
+                  <option value="wantToRead">Want to Read</option>
+                  <option value="read">Read</option>
+                  <option value="none">None</option>
+                </select>
+              </div>
+            </div>
+            <div className="book-title">{book.title}</div>
+            <div className="book-authors">{book.authors[0]}</div>
+          </div>
         </li>
-
       )
+    }))
+  }
 
-      })
-    )
+  handleSearch(query) {
+    BooksAPI.search(query, 10).then((results) => {
+      if (results.length > 0) {
+        this.setState((state) => ({searchResults: results}))
+      } else {
+        console.log('No Books found!')
+      }
+    })
+  }
+
+  handleKeyPress(e) {
+    if (e.key === 'Enter') {
+      this.handleSearch(e.target.value)
+    }
   }
 
   render() {
     return (
       <div className="app">
-          <Route path="/search" render={() => (
-            <div className="search-books">
+
+        <Route path="/search" render={() => (
+          <div className="search-books">
             <div className="search-books-bar">
-            <Link className="close-search" to="/" >Close</Link>
-            <div className="search-books-input-wrapper">
-            {/*
-              NOTES: The search from BooksAPI is limited to a particular set of search terms.
-              You can find these search terms here:
-              https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-              However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-              you don't find a specific author or title. Every search is limited by search terms.
-              */}
-              <input type="text" placeholder="Search by title or author"/>
-
+              <Link className="close-search" to="/">Close</Link>
+              <div className="search-books-input-wrapper">
+                {/* NOTES: The search from BooksAPI is limited to a particular set of search terms. You can find these search terms here: https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if you don't find a specific author or title. Every search is limited by search terms. */}
+                <input type="text" placeholder="Search by title or author" onKeyPress={(e) => this.handleKeyPress(e)}/>
               </div>
-              </div>
-              <div className="search-books-results">
-              <ol className="books-grid"></ol>
-              </div>
-              </div>
-          )}/>
+            </div>
+            <div className="search-books-results">
+              <ol className="books-grid">
+                {this.renderBooks(this.state.searchResults)}
+              </ol>
+            </div>
+          </div>
+        )}/>
 
-          <Route exact path="/" render={() => (
-
+        <Route exact path="/" render={() => (
           <div className="list-books">
             <div className="list-books-title">
               <h1>MyReads</h1>
@@ -100,18 +113,15 @@ class BooksApp extends React.Component {
                   <h2 className="bookshelf-title">Currently Reading</h2>
                   <div className="bookshelf-books">
                     <ol className="books-grid">
-                  {
-                    this.returnBooksFromState('currentlyReading')
-                  }
+                      {this.renderBooks(this.filterBooksUsingShelf(this.state.books, 'currentlyReading'))}
                     </ol>
-
                   </div>
                 </div>
                 <div className="bookshelf">
                   <h2 className="bookshelf-title">Want to Read</h2>
                   <div className="bookshelf-books">
                     <ol className="books-grid">
-                      {this.returnBooksFromState('wantToRead')}
+                      {this.renderBooks(this.filterBooksUsingShelf(this.state.books, 'wantToRead'))}
                     </ol>
                   </div>
                 </div>
@@ -119,7 +129,7 @@ class BooksApp extends React.Component {
                   <h2 className="bookshelf-title">Read</h2>
                   <div className="bookshelf-books">
                     <ol className="books-grid">
-                      {this.returnBooksFromState('read')}
+                      {this.renderBooks(this.filterBooksUsingShelf(this.state.books, 'read'))}
                     </ol>
                   </div>
                 </div>
@@ -130,6 +140,7 @@ class BooksApp extends React.Component {
             </div>
           </div>
         )}/>
+
       </div>
     )
   }
